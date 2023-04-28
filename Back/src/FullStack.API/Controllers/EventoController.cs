60303db267d4957e12961745b146c995ff6954c4
Ruan Mendes Ferreira Santos <ruan1011@hotmail.com>
2023-04-28 16:36:37 -0300

    Seção 2: .NET 5

---
+++ Back/src/FullStack.API/Controllers/EventoController.cs
@@ -0,0 +1,65 @@
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStack.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
+{
+    public IEnumerable <Evento> _evento = new Evento[] {
+        new Evento() { 
+            EventoId = 1,
+            Tema = "Angular 11 e .NET 5",
+            Local = "Belo Horizonte",
+            Lote = "Primeiro Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+            ImagemUrl = "Foto.png"
+        },
+        new Evento() { 
+            EventoId = 2,
+            Tema = "Angular e suas novidades",
+            Local = "São Paulo",
+            Lote = "Segundo Lote",
+            QtdPessoas = 450,
+            DataEvento = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
+            ImagemUrl = "Premiere.png"
+        },
+    };
+
+    public EventoController()
+    {
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _evento;
+    }
+
+    
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+        return _evento.Where(evento => evento.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "exemplo post";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"exemplo put com id = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"exemplo delete com id = {id}";
+    }
+}
